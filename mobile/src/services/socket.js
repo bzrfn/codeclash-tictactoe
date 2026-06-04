10392a66fcf8f99ad35a1072441e0bafce9eed7c
bzrfn <al222310320@gmail.com>
2026-06-03 22:47:56 -0600

    Connect mobile app to Render multiplayer server

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,3 +1,10 @@
 import { io } from 'socket.io-client';
-const url=process.env.EXPO_PUBLIC_SOCKET_URL || 'http://localhost:4000';
-export const socket=io(url,{autoConnect:false,transports:['websocket']});
+
+const url =
+  process.env.EXPO_PUBLIC_SOCKET_URL ||
+  'https://codeclash-tictactoe-server.onrender.com';
+
+export const socket = io(url, {
+  autoConnect: false,
+  transports: ['websocket'],
+});
